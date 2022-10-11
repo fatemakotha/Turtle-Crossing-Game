99b92f4a82a0917a2bad3801519cd95e7f5ce980
fatemakotha <fatema.alam.kotha@gmail.com>
2022-10-11 18:24:26 +0600

    Created turtle object and moved it to the bottom m

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import time
-from turtle import Screen
+from turtle import Screen, Turtle
 from player import Player
 from car_manager import CarManager
 from scoreboard import Scoreboard
@@ -7,6 +7,16 @@ from scoreboard import Scoreboard
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.tracer(0)
+
+tim = Turtle()
+tim.color("black")
+tim.shape("turtle")
+tim.penup()
+tim.goto(x=0, y=-280)
+tim.left(90)
+
+
+
 
 game_is_on = True
 while game_is_on:
